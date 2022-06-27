57b0dafc4976008bb17a11bac56e023e6306e74d
Lucas Lacerda <lucaslacerdacl@gmail.com>
2022-06-27 19:29:59 -0300

    fix: provide

--- src/core/core-bootstrap.ts
+++ src/core/core-bootstrap.ts
@@ -3,13 +3,8 @@ import {Module, ModuleMetadata} from '@nestjs/common';
 import {ResumeInteractorService} from './services/resume-interactor-service';
 
 export const coreModule: ModuleMetadata = {
-  providers: [
-    {
-      useClass: ResumeInteractorService,
-      provide: 'ResumeInteractor',
-    },
-  ],
-  exports: ['ResumeInteractor'],
+  providers: [ResumeInteractorService],
+  exports: [ResumeInteractorService],
 };
 
 @Module(coreModule)
